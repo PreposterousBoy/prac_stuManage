a8b4cdc1b252716f827904adc9f99e23eaa45273
iuAmy <1195594964@qq.com>
2021-02-02 00:51:28 +0800

    继续推进写类

--- lib/Person.cpp
+++ lib/Person.cpp
@@ -4,10 +4,47 @@
 
 
 std::vector<std::string> Student::get_info() const {
-	std::vector<std::string> tmp {
-		get
-	}
+	std::vector<std::string> tmp;
+	tmp.push_back(get_name());
+	tmp.push_back(get_id());
+	tmp.push_back(get_phone());
+	tmp.push_back(get_addr());
+	tmp.push_back(get_email());
+	tmp.push_back(std::string(get_years()));
+	tmp.push_back(std::string(get_sex());
+	return tmp;
 }
+
+inline
+const std::string BaseStudent::get_name() const{
+	return _name;
+}
+
+inline
+const std::string BaseStudent::get_id() const {
+	return _id;
+}
+
+inline
+const std::string BaseStudent::get_phone() const {
+	return phone;
+}
+
+inline
+const std::string BaseStudent::get_email() const {
+	return email;
+}
+
+inline
+const std::size_t BaseStudent::get_years() const {
+	return years;
+}
+
+inline
+const Sex BaseStudent::get_sex() const {
+	return sex;
+}
+
 
 
 
--- lib/Person.hpp
+++ lib/Person.hpp
@@ -5,31 +5,45 @@
 #include<vector>
 
 
+enum class Sex { Femal, Male, Unkown};
 
 class BaseStudent{
 public:
 	BaseStudent() = default;
-	BaseStudent(std::string& nn, std::string iid):_name(nn), _id(iid){
-	}
+	BaseStudent(std::string &nn, std::string &iid, std::string &add, 
+			std::string &pp, std::string &emm, std::size_t yy, Sex ss)
+			:_name(nn), _id(iid), addr(add), phone(pp), years(yy), sex(ss){}
+	BaseStudent(std::string &nn, std::string &idd, std::string add ="", std::string pp = "",
+			std::string emm = "", std::size_t yy = 0, Sex ss = Sex::Unkown)
+			:_name(nn), _id(idd), addr(add), phone(pp), email(emm), years(yy), sex(ss) {}
 
-	virtual std::vector<std::string> get_info() const = 0;
-	
-	virtual std::string get_name() const;
 
-	virtual std::string get_id() const;
-
-	virtual std::string get_phone() const;
-	virtual std::string get_addr() const;
-
+	virtual std::vector<std::string> get_info() const;
+	virtual const std::string get_name() const;
+	virtual const std::string get_id() const;
+	virtual const std::string get_phone() const; // ?? 返回值 const和 成员函数const 两者 冲突 和关系是什么？
+	virtual const std::string get_addr() const;
+	virtual const std::string get_email() const;
+	virtual const std::size_t get_years() const;
+	virtual const Sex get_sex() const;
+	virtual int set_phone(std::string &pp);
+	virtual int set_addr(std::string &add);
+	virtual int set_email(std::string &eemail);
+	virtual int set_years(std::size_t yy);
+	virtual int set_Sex(Sex ss);
 	virtual ~BaseStudent() = default;
 private:
 	const std::string _name;
 	const std::string _id;
 	std::string addr;
 	std::string phone;
+	std::string email;
+	std::size_t years;
+	Sex sex;
+
 };
 
-
+bool operator==(BaseStudent &a, BaseStudent &b);
 
 class Student: public BaseStudent{
 public:
