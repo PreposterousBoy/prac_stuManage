a7c0e6c1107ee591e3b1b1762e8b60a47bceceb3
iuAmy <1195594964@qq.com>
2021-02-18 20:19:34 +0800

    一个简单的log class

---
+++ Log.hpp
@@ -0,0 +1,56 @@
+#ifndef __LOG_H
+#define __LOG_H
+
+
+#include<iostream>
+
+namespace stuSys {
+
+class Log {
+public:
+	Log();
+	virtual ~Log();
+	
+	std::ostring
+
+
+
+
+public:
+
+
+protect:
+
+
+
+private:
+
+};
+
+
+
+
+
+
+
+
+
+class LogEvent {
+public:
+	
+
+};
+
+
+
+
+
+
+
+
+
+
+};
+
+
+#endif /* __LOG_H */
